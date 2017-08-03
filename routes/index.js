81f34e5365e129f080c0cae029393016ab87d9b4
lyubomyr <lyubomyr.fedevitch@eliftech.com>
2017-08-03 15:25:19 +0300

    receive binary data

--- routes/index.js
+++ routes/index.js
@@ -9,13 +9,25 @@ var fs = require('fs');
 router.post('/', upload.array(), (req, res, next) => {
   console.log(req.headers['user-agent'], " - data received: ", req.body);
 var date = new Date();
-  fs.appendFile('data.txt', "\n" + date.toISOString() + " " + JSON.stringify(req.body), function(err){
-if(err) throw err;
 
-console.log('data saved');
-})	
+var binary = [];
+res.on('data', function(chunk){
+  binary.push(chunk);
+}).on('end', function() {
+        var buffer = Buffer.concat(binary);
+        fs.appendFile('data.txt', "\n" + date.toISOString() + " " + buffer.toJSON(), function(err){
+          if(err) throw err;
+
+          console.log('data saved');
+        })
+    });
+  fs.appendFile('data.txt', "\n" + date.toISOString() + " " + JSON.stringify(req.body), function(err){
+    if(err) throw err;
+
+    console.log('data saved');
+  })
   res.status(200).send({
-    serverResponse: "Command received"
+    serverResponse: "Log info received"
   });
 });
 
